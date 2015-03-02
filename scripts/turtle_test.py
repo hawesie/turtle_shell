526c71b8c35fb1ed8d9fdfad59d0c1efb00552ab
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-03-02 22:15:28 +0000

    Simplified example

--- scripts/turtle_test.py
+++ scripts/turtle_test.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
-import rospy
 
 from turtle_shell import turtlebot
 
-if __name__ == '__main__':
-    try:
-  
-        (x,y) = turtlebot.position()
-        print(x, y)
-        # turtlebot.rotate(90)        
-        # turtlebot.goto(x + 2, y)
-        turtlebot.goto(0, 0)
-        (x,y) = turtlebot.position()
-        print(x, y)
+(x,y) = turtlebot.position()
+print(x, y)
+# turtlebot.rotate(90)        
+turtlebot.goto(x + 2, y)
+# turtlebot.goto(0, 0)
+(x,y) = turtlebot.position()
+print(x, y)
 
-    except rospy.ROSInterruptException:
-        pass
